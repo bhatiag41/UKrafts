a51560a5032faad833286e278da76024de315381
Radhika Agarwal <radhikaag393@gmail.com>
2023-11-21 01:16:26 +0530

    initial commit

---
+++ src/data.js
@@ -0,0 +1,25 @@
+export default[
+    {
+        title:"Aipan",
+        Price:"250",
+        description:"Aipan is a traditional folk art form originating from the Kumaon region of Uttarakhand, India. This intricate and colorful art is often created on walls and floors, serving as a form of decoration during festivals, weddings, and other special occasions..",
+        img:"assets/Aipan.jpeg.jpeg"
+    },
+    //
+    //{
+      //  title:"Pahari Topi",
+        
+       //date:"3-october-2023 - 10-october-2023",
+        //description:"Ladakh is the highest plateau in India with most of it being over 3,000 m (9,800 ft). It extends from the Himalayan to the Kunlun Ranges",
+        //img:"https://assets.cntraveller.in/photos/60ba11f73ff1eb236f5a730c/16:9/w_1024%2Cc_limit/LadakhLead.jpg"
+    //},
+    //{
+      //  title:"Nara Park",
+        //loc:"Japan",
+        //gmap:"https://maps.app.goo.gl/AcpioSeSnnUNo6qr7",
+        //date:"10-september-2023 - 15-september-2023",
+        //description:"Nara Park is a public park located in the city of Nara, Japan, at the foot of Mount Wakakusa. Established in 1880, it is one of the oldest parks in Japan. Administratively, the park is under the control of Nara Prefecture.",
+        //img:"https://lp-cms-production.imgix.net/image_browser/shutterstockRF_168163925.jpg?auto=format&q=75"
+    //},
+
+]
