ced4e0dc2de11de8b1c984816396691a6e9ca19f
Gaurav Bhatia <115914823+bhatiag41@users.noreply.github.com>
2023-11-20 23:34:21 +0530

    tweak 1

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {BrowserRouter as Router } from 'express';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import './App.css';
 import Home from "components/Home.js"
 import ProductDetails from "components/ProductDetails.js"
