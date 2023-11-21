5126bdb73206156d788f00ee8e1a40431778f1b9
Gaurav Bhatia <115914823+bhatiag41@users.noreply.github.com>
2023-11-21 10:22:58 +0530

    update

--- src/components/Home.js
+++ src/components/Home.js
@@ -176,43 +176,6 @@ export default function Home() {
                 <div class="dropdown-panel">
                   <ul class="dropdown-panel-list">
                     <li class="menu-title">
-                      <a href="/">Electronics</a>
-                    </li>
-
-                    <li class="panel-list-item">
-                      <a href="/">Desktop</a>
-                    </li>
-
-                    <li class="panel-list-item">
-                      <a href="/">Laptop</a>
-                    </li>
-
-                    <li class="panel-list-item">
-                      <a href="/">Camera</a>
-                    </li>
-
-                    <li class="panel-list-item">
-                      <a href="/">Tablet</a>
-                    </li>
-
-                    <li class="panel-list-item">
-                      <a href="/">Headphone</a>
-                    </li>
-
-                    <li class="panel-list-item">
-                      <a href="/">
-                        <img
-                          src="assets/electronics-banner-1.jpg"
-                          alt="headphone collection"
-                          width="250"
-                          height="119"
-                        />
-                      </a>
-                    </li>
-                  </ul>
-
-                  <ul class="dropdown-panel-list">
-                    <li class="menu-title">
                       <a href="/">Aipan</a>
                     </li>
 
@@ -274,7 +237,7 @@ export default function Home() {
                   </ul>
 
                   
-                  // <ul class="dropdown-panel-list">
+                  {/* // <ul class="dropdown-panel-list">
                   //   <li class="menu-title">
                   //     <a href="/">Men's</a>
                   //   </li>
@@ -346,45 +309,8 @@ export default function Home() {
                   //       />
                   //     </a>
                   //   </li>
-                  // </ul>
-                  
-
-                  <ul class="dropdown-panel-list">
-                    <li class="menu-title">
-                      <a href="/">Electronics</a>
-                    </li>
-
-                    <li class="panel-list-item">
-                      <a href="/">Smart Watch</a>
-                    </li>
-
-                    <li class="panel-list-item">
-                      <a href="/">Smart TV</a>
-                    </li>
-
-                    <li class="panel-list-item">
-                      <a href="/">Keyboard</a>
-                    </li>
-
-                    <li class="panel-list-item">
-                      <a href="/">Mouse</a>
-                    </li>
-
-                    <li class="panel-list-item">
-                      <a href="/">Microphone</a>
-                    </li>
-
-                    <li class="panel-list-item">
-                      <a href="/">
-                        <img
-                          src="assets/electronics-banner-2.jpg"
-                          alt="mouse collection"
-                          width="250"
-                          height="119"
-                        />
-                      </a>
-                    </li>
-                  </ul>
+                  // </ul>}
+                   */}
                 </div>
               </li>
 
